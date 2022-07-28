41e6b1e3e80b33ada9aceed401a89e00e6e3ce0b
danielzarateos <60785248+danielzarateos@users.noreply.github.com>
2022-07-28 08:13:22 -0500

    .

---
+++ ClassesAndObjects/ClassMeaning.java
@@ -0,0 +1,9 @@
+package ClassesAndObjects;
+
+public class ClassMeaning {
+/* Let's say we want to create a datatype, we have to create a XXX class, the blueprint
+that shows how's the data of this datatype is gonna be distributed
+
+String, number, boolean can all be combined in a class. We can even introduce functions inside the class
+calculating atributes inside itself */    
+}
---
+++ ClassesAndObjects/ObjectMeaning.java
@@ -0,0 +1,8 @@
+package ClassesAndObjects;
+
+public class ObjectMeaning {
+    /* An object is an instance of the class, is the actual thing 
+     * the program is going to work with 
+     */
+    
+}
