e77ca4c3ed4a40ded4044665840e665f1825463e
danielzarateos <60785248+danielzarateos@users.noreply.github.com>
2022-07-29 12:03:42 -0500

    .

---
+++ TomaDeRequerimientosLoccitane/Requerimientos.java
@@ -0,0 +1,32 @@
+package TomaDeRequerimientosLoccitane;
+
+public class Requerimientos {
+    /*
+     * Puntos de venta
+     * Recogida en tienda
+     * Actualizacion de datos 
+     * 
+     * Back y frontend de las actividades de venta o de paginas nuevas, necesitan soporte y desarrollo
+     * 
+     * Montar videos al backend y ajustarlo a pantalla y la capacidad de responsibity de la pagina
+     * INCRUSTADO, no desde shopitfy
+     * 
+     * Dentro de shopify redirige a youtube, buscar la manera de integrar video/gif a banners dentro de el home de
+     * shoppify
+     * 
+     * ------------ checkout ----------------------------------------------
+     * Escoger muestras que le llegan al cliente que compra
+     * 
+     * ruletas y actividades interactivas, llevada de datos a SAP desde el checkout form 
+     * 
+     * Analisis de competencia a la hora del desarrollo web
+     * 
+     * 
+     * 
+     * Propuesta de ciencia de datos
+     * 
+     * Comerssia sistema POS 
+     * 
+     * 
+     */
+}
